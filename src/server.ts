f430af3886b51feda00e508c373e334b32d0a82d
John Tucker <jtucker@appirio.com>
2019-08-05 19:03:07 -0400

    config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+/* eslint no-console: 0 */
 import cors from 'cors';
 import express from 'express';
 
 const app = express();
 app.use(cors());
-app.get('/', (req, res) => res.send({ hello: 'world' }));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get('/', (req, res): void => {
+  res.send({ hello: 'world' });
+});
+app.listen(3000, (): void => console.log('Example app listening on port 3000!'));
